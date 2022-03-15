83b22feb57b5d77cfa7de4123eafc5e6ff6ecce9
Mabontle22 <tebogo.ramoretli1998@gmail.com>
2022-03-14 21:08:33 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <time.h>
 include <stdio.h>
@@ -15,21 +16,19 @@ int n:
 srans(time(0)):
 n = rand() - RAND_MAX / 2:
 
-if (n==0)
-{
-print("%i is negative\n",n):
-}
-
-else if (n<0)
+if (n == 0)
 {
 print("%i is zero\n",n):
 }
 
+else if (n < 0)
+{
+print("%i is possitive\n",n):
+}
+
 else
 {
-printf("%i is possitive\n",n):
+printf("%i is positive\n",n):
 }
 
 return (0):
-
-}
